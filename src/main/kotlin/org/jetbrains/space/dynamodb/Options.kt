7247c6e2c8b3b79f550dda60a4617785891ef513
Kirill <bitikit@gmail.com>
2021-09-15 02:08:35 +0300

    Add docs to table and options

--- src/main/kotlin/org/jetbrains/space/dynamodb/Options.kt
+++ src/main/kotlin/org/jetbrains/space/dynamodb/Options.kt
@@ -2,23 +2,40 @@ package org.jetbrains.space.dynamodb
 
 import java.nio.file.Path
 
+/**
+ * Represents options for work.
+ */
 data class Options(
     val database: DatabaseInfo,
     val table: TableInfo,
     val csvPath: Path
 ) {
+    /**
+     * Information to connect a database.
+     */
     data class DatabaseInfo(
         val url: String,
         val user: String,
         val password: String
     )
 
+    /**
+     * Information about a table inside a database.
+     */
     data class TableInfo(
         val name: String,
         val schema: String
     )
 
     companion object {
+        /**
+         * Parses command line arguments and reads the left information from a console.
+         * All info cannot be passed as arguments for security reasons.
+         *
+         * Format of args: `path/to/csv/file table_name schema_name`
+         *
+         * After that, you should enter an URL, username and password for your [DatabaseInfo].
+         */
         fun parseAndReadOptions(commandArgs: List<String>): Options {
             require(commandArgs.size == 3) {
                 "You have to provide exactly 3 arguments: path to CSV, table name, schema name."
--- src/main/kotlin/org/jetbrains/space/dynamodb/Table.kt
+++ src/main/kotlin/org/jetbrains/space/dynamodb/Table.kt
@@ -6,6 +6,11 @@ import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.file.Path
 
+/**
+ * Represents a simple table with [columnsNames] and its [values].
+ * The size of each row in [values] must match with [columnsNames] length.
+ * Also, size of [columnsNames] must be non-zero.
+ */
 data class Table(
     val columnsNames: List<String>,
     val values: List<List<String>>
@@ -22,6 +27,10 @@ data class Table(
     }
 
     companion object {
+        /**
+         * Tries to parse CSV file from [path].
+         * @throws IllegalArgumentException if the file cannot be parsed or contains no rows.
+         */
         fun parseFromCsv(path: Path): Table {
             val parser = CSVParser.parse(path, Charset.defaultCharset(), CSVFormat.DEFAULT)
             val records = try {
@@ -29,7 +38,7 @@ data class Table(
             } catch (e: IOException) {
                 throw IllegalArgumentException("Couldn't parse CSV file from '$path'", e)
             }
-            require(records.isNotEmpty()) { "CSV file must contain at lease one row." }
+            require(records.isNotEmpty()) { "CSV file must contain at least one row." }
             return Table(
                 records.first().toList(),
                 records.drop(1).map { it.toList() }
