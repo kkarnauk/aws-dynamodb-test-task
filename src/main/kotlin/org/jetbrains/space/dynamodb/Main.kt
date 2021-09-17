c019780c3f3a371dfd11a0da7ae7fb117af982d7
Kirill <bitikit@gmail.com>
2021-09-17 17:51:57 +0300

    Uppercase columns names in table

--- src/main/kotlin/org/jetbrains/space/dynamodb/Main.kt
+++ src/main/kotlin/org/jetbrains/space/dynamodb/Main.kt
@@ -3,7 +3,8 @@ package org.jetbrains.space.dynamodb
 fun main(args: Array<String>) {
     val options = Options.parseAndReadOptions(args.toList())
     val table = Table.parseFromCsv(options.csvPath)
+    val dbTable = Table(table.columnsNames.map { it.uppercase() }, table.values)
     HSQLDatabase.connect(options.database)?.use { database ->
-        database.createTableOrAppend(options.table, table)
+        database.createTableOrAppend(options.table, dbTable)
     } ?: println("Couldn't connect to your database.")
 }
